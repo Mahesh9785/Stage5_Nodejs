4a5d140298921af0e69b2bb1468e9b07e1745d7b
Mahesh <Mahesh9785>
2023-02-07 17:15:03 +0530

    Further Practice

---
+++ factorial.js
@@ -0,0 +1,20 @@
+module.exports.factorial=function (a){
+    var result=1;
+    for(var i=1;i<=a;i++){
+        result=result *i;
+    }
+    console.log(result);
+}
+
+
+
+// var result=1;
+// const factorial=(a)=>{
+//     for(var i=1;i<=a;i++){
+//         result=result *i;
+//     }
+//     return result;
+// }
+
+// exports.out = factorial(5);
+
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+fact=require('./factorial');
+
 console.log("Hello World");
 var obj = {
     authorName: 'Ryan Dahl',
     language: 'Node.js'
 }
 console.log(obj);
-function Display(x) { 
-    console.log(x);
-}
 
-Display(100);
+fact.factorial(5);
+
+var myLog = require('./utility');
+
+myLog.info('Node.js Started');
+myLog.warning('Unable to process request');
+
+var myPerson=require('./Person');
+
+var Person1=new myPerson('Mahesh','Sharma');
+console.log(Person1.fullName());
+
+var fs = require('fs');
+
+var data = fs.readFileSync('Test.txt', 'utf8');
+console.log(data);
+
