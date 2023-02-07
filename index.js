0c1aa1a00c614173bb4e5c447b8b64104e1853e5
Mahesh <Mahesh9785>
2023-02-07 13:28:00 +0530

    Nodejs practice

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+var obj = {
+    authorName: 'Ryan Dahl',
+    language: 'Node.js'
+}
+console.log(obj);
+function Display(x) { 
+    console.log(x);
+}
+
+Display(100);
