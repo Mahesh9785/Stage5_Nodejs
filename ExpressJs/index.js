33fc5d59906be0157692af290feac3867972ef96
Mahesh <Mahesh9785>
2023-02-10 18:44:47 +0530

    Done with NodeJS

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,41 +1,6 @@
 var express=require('express');
 const app=express();
-const {MongoClient} = require('mongodb');
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    //const uri = "mongodb+srv://Mind-Your-Business:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
- 
-
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-
-main().catch(console.error);
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
